d5e79856a79f50b9f0dff4757f668ad55ed2e6eb
AssanJ <84403654+assanj@users.noreply.github.com>
2026-08-31 19:27:10 +0300

    Update ccd_claim_policy_incident_exposure_transaction.sql

--- ccd_claim_policy_incident_exposure_transaction.sql
+++ ccd_claim_policy_incident_exposure_transaction.sql
@@ -1,40 +1,110 @@
-2026/08/31 15:36:30 - Spoon - Using legacy execution engine
-2026/08/31 15:36:31 - Spoon - Transformation opened.
-2026/08/31 15:36:31 - Spoon - Launching transformation [xls2edw_ref_xls4triangles]...
-2026/08/31 15:36:31 - Spoon - Started the transformation execution.
-2026/08/31 15:36:32 - xls2edw_ref_xls4triangles - Dispatching started for transformation [xls2edw_ref_xls4triangles]
-2026/08/31 15:36:32 - ref_xls4triangles_regions.0 - Connected to database [EDW] (commit=1000)
-2026/08/31 15:36:32 - Регионы.0 - ERROR (version 8.2.0.0-342, build 8.2.0.0-342 from 2018-11-14 10.30.55 by buildguy) : Error processing row from Excel file [file:////qs9/qfs_Actuary/Справочники для треугольников.xlsx] : org.pentaho.di.core.exception.KettleException: 
-2026/08/31 15:36:32 - Регионы.0 - org.apache.poi.openxml4j.exceptions.InvalidOperationException: Can't open the specified file input stream from file: 'file:\qs9\qfs_Actuary\Справочники для треугольников.xlsx'
-2026/08/31 15:36:32 - Регионы.0 - Can't open the specified file input stream from file: 'file:\qs9\qfs_Actuary\Справочники для треугольников.xlsx'
-2026/08/31 15:36:32 - Регионы.0 - ERROR (version 8.2.0.0-342, build 8.2.0.0-342 from 2018-11-14 10.30.55 by buildguy) : org.pentaho.di.core.exception.KettleException: 
-2026/08/31 15:36:32 - Регионы.0 - org.apache.poi.openxml4j.exceptions.InvalidOperationException: Can't open the specified file input stream from file: 'file:\qs9\qfs_Actuary\Справочники для треугольников.xlsx'
-2026/08/31 15:36:32 - Регионы.0 - Can't open the specified file input stream from file: 'file:\qs9\qfs_Actuary\Справочники для треугольников.xlsx'
-2026/08/31 15:36:32 - Регионы.0 - 
-2026/08/31 15:36:32 - Регионы.0 - 	at org.pentaho.di.trans.steps.excelinput.staxpoi.StaxPoiWorkbook.<init>(StaxPoiWorkbook.java:81)
-2026/08/31 15:36:32 - Регионы.0 - 	at org.pentaho.di.trans.steps.excelinput.WorkbookFactory.getWorkbook(WorkbookFactory.java:43)
-2026/08/31 15:36:32 - Регионы.0 - 	at org.pentaho.di.trans.steps.excelinput.ExcelInput.getRowFromWorkbooks(ExcelInput.java:552)
-2026/08/31 15:36:32 - Регионы.0 - 	at org.pentaho.di.trans.steps.excelinput.ExcelInput.processRow(ExcelInput.java:432)
-2026/08/31 15:36:32 - Регионы.0 - 	at org.pentaho.di.trans.step.RunThread.run(RunThread.java:62)
-2026/08/31 15:36:32 - Регионы.0 - 	at java.lang.Thread.run(Thread.java:748)
-2026/08/31 15:36:32 - Регионы.0 - Caused by: org.apache.poi.openxml4j.exceptions.InvalidOperationException: Can't open the specified file input stream from file: 'file:\qs9\qfs_Actuary\Справочники для треугольников.xlsx'
-2026/08/31 15:36:32 - Регионы.0 - 	at org.apache.poi.openxml4j.opc.ZipPackage.openZipEntrySourceStream(ZipPackage.java:155)
-2026/08/31 15:36:32 - Регионы.0 - 	at org.apache.poi.openxml4j.opc.ZipPackage.<init>(ZipPackage.java:142)
-2026/08/31 15:36:32 - Регионы.0 - 	at org.apache.poi.openxml4j.opc.ZipPackage.<init>(ZipPackage.java:117)
-2026/08/31 15:36:32 - Регионы.0 - 	at org.apache.poi.openxml4j.opc.OPCPackage.open(OPCPackage.java:257)
-2026/08/31 15:36:32 - Регионы.0 - 	at org.apache.poi.openxml4j.opc.OPCPackage.open(OPCPackage.java:187)
-2026/08/31 15:36:32 - Регионы.0 - 	at org.pentaho.di.trans.steps.excelinput.staxpoi.StaxPoiWorkbook.<init>(StaxPoiWorkbook.java:78)
-2026/08/31 15:36:32 - Регионы.0 - 	... 5 more
-2026/08/31 15:36:32 - Регионы.0 - Caused by: java.io.FileNotFoundException: file:\qs9\qfs_Actuary\Справочники для треугольников.xlsx (Синтаксическая ошибка в имени файла, имени папки или метке тома)
-2026/08/31 15:36:32 - Регионы.0 - 	at java.io.FileInputStream.open0(Native Method)
-2026/08/31 15:36:32 - Регионы.0 - 	at java.io.FileInputStream.open(FileInputStream.java:195)
-2026/08/31 15:36:32 - Регионы.0 - 	at java.io.FileInputStream.<init>(FileInputStream.java:138)
-2026/08/31 15:36:32 - Регионы.0 - 	at org.apache.poi.openxml4j.opc.ZipPackage.openZipEntrySourceStream(ZipPackage.java:152)
-2026/08/31 15:36:32 - Регионы.0 - 	... 10 more
-2026/08/31 15:36:32 - Регионы.0 - Finished processing (I=0, O=0, R=0, W=0, U=0, E=1)
-2026/08/31 15:36:32 - xls2edw_ref_xls4triangles - Transformation detected one or more steps with errors.
-2026/08/31 15:36:32 - xls2edw_ref_xls4triangles - Transformation is killing the other steps!
-2026/08/31 15:36:33 - xls2edw_ref_xls4triangles - ERROR (version 8.2.0.0-342, build 8.2.0.0-342 from 2018-11-14 10.30.55 by buildguy) : Errors detected!
-2026/08/31 15:36:33 - Spoon - The transformation has finished!!
-2026/08/31 15:36:33 - xls2edw_ref_xls4triangles - ERROR (version 8.2.0.0-342, build 8.2.0.0-342 from 2018-11-14 10.30.55 by buildguy) : Errors detected!
-2026/08/31 15:36:33 - xls2edw_ref_xls4triangles - ERROR (version 8.2.0.0-342, build 8.2.0.0-342 from 2018-11-14 10.30.55 by buildguy) : Errors detected!
+@echo off
+REM
+REM Licensed to the Apache Software Foundation (ASF) under one or more
+REM contributor license agreements.  See the NOTICE file distributed with
+REM this work for additional information regarding copyright ownership.
+REM The ASF licenses this file to You under the Apache License, Version 2.0
+REM (the "License"); you may not use this file except in compliance with
+REM the License.  You may obtain a copy of the License at
+REM
+REM       http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM Unless required by applicable law or agreed to in writing, software
+REM distributed under the License is distributed on an "AS IS" BASIS,
+REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM See the License for the specific language governing permissions and
+REM limitations under the License.
+REM
+
+setlocal
+
+REM switch to script directory
+cd /D %~dp0
+
+REM Option to change the Characterset of the Windows Shell to show foreign caracters
+if not "%HOP_WINDOWS_SHELL_ENCODING%"=="" chcp %HOP_WINDOWS_SHELL_ENCODING%
+
+set LIBSPATH=lib\core
+set CLASSPATH=lib\core\*;lib\spark-client\*;lib\swt\win64\*
+
+REM Optional versioned Spark client pack
+if defined HOP_SPARK_CLIENT_VERSION if exist "lib\spark-clients\%HOP_SPARK_CLIENT_VERSION%\" (
+  set CLASSPATH=%CLASSPATH%;lib\spark-clients\%HOP_SPARK_CLIENT_VERSION%\*
+)
+
+
+
+
+
+:NormalStart
+REM set java primary is HOP_JAVA_HOME fallback to JAVA_HOME or default java
+if not "%HOP_JAVA_HOME%"=="" (
+    set _HOP_JAVA="%HOP_JAVA_HOME%\bin\java"
+) else if not "%JAVA_HOME%"=="" (
+    set _HOP_JAVA="%JAVA_HOME%\bin\java"
+) else (
+    set _HOP_JAVA=java
+)
+
+REM # Settings for all OSses
+
+if "%HOP_OPTIONS%"=="" set HOP_OPTIONS=-Xmx2048m
+
+REM
+REM If the user passes in DEBUG as the first parameter, it starts Hop in debugger mode and opens port 5009
+REM to allow attaching a debugger to step code.
+if [%1]==[DEBUG] (
+REM # optional line for attaching a debugger
+set HOP_OPTIONS=%HOP_OPTIONS% -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5009)
+
+REM Pass HOP variables if they're set.
+if not "%HOP_AUDIT_FOLDER%"=="" (
+  set HOP_OPTIONS=%HOP_OPTIONS% -DHOP_AUDIT_FOLDER="%HOP_AUDIT_FOLDER%"
+) else (
+    set HOP_OPTIONS=%HOP_OPTIONS% -DHOP_AUDIT_FOLDER=.\audit
+)
+if not "%HOP_CONFIG_FOLDER%"=="" (
+  set HOP_OPTIONS=%HOP_OPTIONS% -DHOP_CONFIG_FOLDER="%HOP_CONFIG_FOLDER%"
+)
+if not "%HOP_SHARED_JDBC_FOLDERS%"=="" (
+  set HOP_OPTIONS=%HOP_OPTIONS% -DHOP_SHARED_JDBC_FOLDERS="%HOP_SHARED_JDBC_FOLDERS%"
+)
+if not "%HOP_PLUGIN_BASE_FOLDERS%"=="" (
+  set HOP_OPTIONS=%HOP_OPTIONS% -DHOP_PLUGIN_BASE_FOLDERS="%HOP_PLUGIN_BASE_FOLDERS%"
+)
+if not "%HOP_PASSWORD_ENCODER_PLUGIN%"=="" (
+  set HOP_OPTIONS=%HOP_OPTIONS% -DHOP_PASSWORD_ENCODER_PLUGIN=%HOP_PASSWORD_ENCODER_PLUGIN%
+)
+if not "%HOP_AES_ENCODER_KEY%"=="" (
+  set HOP_OPTIONS=%HOP_OPTIONS% -DHOP_AES_ENCODER_KEY=%HOP_AES_ENCODER_KEY%
+)
+if not "%HOP_AES_ENCODER_KEY_FILE%"=="" (
+  set HOP_OPTIONS=%HOP_OPTIONS% -DHOP_AES_ENCODER_KEY_FILE=%HOP_AES_ENCODER_KEY_FILE%
+)
+
+set HOP_OPTIONS=%HOP_OPTIONS% -DHOP_PLATFORM_OS=Windows
+set HOP_OPTIONS=%HOP_OPTIONS% -DHOP_PLATFORM_RUNTIME=Server
+set HOP_OPTIONS=%HOP_OPTIONS% -DHOP_AUTO_CREATE_CONFIG=Y
+set HOP_OPTIONS=%HOP_OPTIONS% --add-opens java.xml/jdk.xml.internal=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.invoke=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED --add-opens java.base/java.net=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.util.concurrent=ALL-UNNAMED --add-opens java.base/java.util.concurrent.atomic=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.base/sun.nio.cs=ALL-UNNAMED --add-opens java.base/sun.security.action=ALL-UNNAMED --add-opens java.base/sun.util.calendar=ALL-UNNAMED --add-opens java.security.jgss/sun.security.krb5=ALL-UNNAMED --add-exports java.base/sun.nio.ch=ALL-UNNAMED
+
+echo ===[Environment Settings - hop-server.bat]====================================
+echo.
+echo Java identified as %_HOP_JAVA%
+echo.
+echo HOP_OPTIONS=%HOP_OPTIONS%
+echo.
+echo.
+rem ===[Collect command line arguments...]======================================
+if [%1]==[DEBUG] (
+FOR /f "tokens=1*" %%x IN ("%*") DO set _cmdline=%%y
+GOTO Run
+)
+set _cmdline=%*
+
+:Run
+echo Command to start Hop will be:
+echo %_HOP_JAVA% -classpath %CLASSPATH% -Djava.library.path=%LIBSPATH% %HOP_OPTIONS% org.apache.hop.www.HopServer %_cmdline%
+echo.
+echo ===[Starting HopServer]=========================================================
+
+%_HOP_JAVA% -classpath %CLASSPATH% -Djava.library.path=%LIBSPATH% %HOP_OPTIONS% org.apache.hop.www.HopServer %_cmdline%
