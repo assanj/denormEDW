d1975232899724fae0cd67924857e55b8eda6d0e
AssanJ <84403654+assanj@users.noreply.github.com>
2026-08-31 19:46:36 +0300

    Update ccd_claim_policy_incident_exposure_transaction.sql

--- ccd_claim_policy_incident_exposure_transaction.sql
+++ ccd_claim_policy_incident_exposure_transaction.sql
@@ -1,16 +1,3 @@
-    },
-    {
-      "name" : "HOP_PASSWORD_ENCODER_PLUGIN",
-      "value" : "Hop",
-      "description" : "Specifies the password encoder plugin to use by ID (Hop is the default)."
-    },
-    {
-      "name" : "HOP_AES_ENCODER_KEY",
-      "value" : "",
-      "description" : "Key used by the AES/AES2 password encoder plugins. Can also be set as a system property."
-    },
-    {
-      "name" : "HOP_AES_ENCODER_KEY_FILE",
-      "value" : "",
-      "description" : "Path to a file containing the AES/AES2 password encoder key. Used when HOP_AES_ENCODER_KEY is empty."
-    },
+echo %HOP_OPTIONS%
+echo %HOP_WEB_ADMIN_PASSWORD%
+echo %HOP_WEB_ADMIN_USER%
