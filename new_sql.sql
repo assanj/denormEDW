ee7ef5f3701d5c004f1f7ff99bcd0653361d65c0
AssanJ <84403654+assanj@users.noreply.github.com>
2026-09-01 16:59:46 +0300

    Refactor SQL calculation for normalized loss count

--- new_sql.sql
+++ new_sql.sql
@@ -1,3 +1,12 @@
 "C19_Нормализованное количество убытков" =
 Sum([Количество убытков]*[Коэф_развития_1]*if([Тип убытка крупный]='ППВУ',0,1)*if(isnull(k_covid19_osago),1,k_covid19_osago)/(if(isnull(K_risk),1,K_risk)*K_season))
 
+
+
+    SUM(
+        количество_убытков 
+        * коэф_развития_1 
+        * CASE WHEN тип_убытка_крупный = 'ППВУ' THEN 0 ELSE 1 END
+        * COALESCE(k_covid19_osago, 1)
+        / (COALESCE(k_risk, 1) * k_season)
+    ) AS c19_нормализованное_количество_убытков
