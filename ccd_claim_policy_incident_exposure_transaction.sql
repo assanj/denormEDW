6316a93c525ec458e47032fb1bb1d919167024c7
AssanJ <84403654+assanj@users.noreply.github.com>
2026-09-01 13:39:31 +0300

    Update ccd_claim_policy_incident_exposure_transaction.sql

--- ccd_claim_policy_incident_exposure_transaction.sql
+++ ccd_claim_policy_incident_exposure_transaction.sql
@@ -1,9 +1,121 @@
-=if([Тип убытка крупный]='Традиция' or [Тип убытка крупный]='ОПВУ','ОтвСтр',if([Тип убытка крупный]='ППВУ','ПрСтр','-'))
+Sub Расчет_по_всем()
 
+' Расчет 1: ответственный страховщик, ФЛ, первоначальные
 
+Sheets("DataFull").Select
+ActiveSheet.Range("$A$1:$G$1873").AutoFilter Field:=1, Criteria1:="ОтвСтр"
+ActiveSheet.Range("$A$1:$G$1873").AutoFilter Field:=2, Criteria1:="ФЛ"
+ActiveSheet.Range("$A$1:$G$1873").AutoFilter Field:=3, Criteria1:="Первоначальный"
+Range("D1:G1").Select
+Range(Selection, Selection.End(xlDown)).Select
+Application.CutCopyMode = False
+Selection.Copy
+Sheets("Data").Select
+Range("A1").Select
+Selection.PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks:=False, Transpose:=False
 
-CASE 
-    WHEN Тип_убытка_крупный IN ('Традиция', 'ОПВУ') THEN 'ОтвСтр'
-    WHEN Тип_убытка_крупный = 'ППВУ' THEN 'ПрСтр'
-    ELSE '-'
-END
+Calc_Ind
+Расчет
+
+Sheets("Result").Select
+Range("A2:D14").Select
+Selection.Copy
+Sheets("ResultFull").Select
+Range("A4").Select
+Selection.PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks:=False, Transpose:=False
+
+' Расчет 2: ответственный страховщик, ФЛ, возобновлённые
+
+Sheets("DataFull").Select
+ActiveSheet.Range("$A$1:$G$1873").AutoFilter Field:=1, Criteria1:="ОтвСтр"
+ActiveSheet.Range("$A$1:$G$1873").AutoFilter Field:=2, Criteria1:="ФЛ"
+ActiveSheet.Range("$A$1:$G$1873").AutoFilter Field:=3, Criteria1:="Возобновлённый"
+Range("D1:G1").Select
+Range(Selection, Selection.End(xlDown)).Select
+Application.CutCopyMode = False
+Selection.Copy
+Sheets("Data").Select
+Range("A1").Select
+Selection.PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks:=False, Transpose:=False
+
+Calc_Ind
+Расчет
+
+Sheets("Result").Select
+Range("A2:D14").Select
+Selection.Copy
+Sheets("ResultFull").Select
+Range("A20").Select
+Selection.PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks:=False, Transpose:=False
+
+' Расчет 3: ответственный страховщик, ФЛ, ЕГП
+
+Sheets("DataFull").Select
+ActiveSheet.Range("$A$1:$G$1873").AutoFilter Field:=1, Criteria1:="ОтвСтр"
+ActiveSheet.Range("$A$1:$G$1873").AutoFilter Field:=2, Criteria1:="ФЛ"
+ActiveSheet.Range("$A$1:$G$1873").AutoFilter Field:=3, Criteria1:="ЕГП"
+Range("D1:G1").Select
+Range(Selection, Selection.End(xlDown)).Select
+Application.CutCopyMode = False
+Selection.Copy
+Sheets("Data").Select
+Range("A1").Select
+Selection.PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks:=False, Transpose:=False
+
+Calc_Ind
+Расчет
+
+Sheets("Result").Select
+Range("A2:D14").Select
+Selection.Copy
+Sheets("ResultFull").Select
+Range("A36").Select
+Selection.PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks:=False, Transpose:=False
+
+' Расчет 4: ответственный страховщик, ЮЛ, первоначальные
+
+Sheets("DataFull").Select
+ActiveSheet.Range("$A$1:$G$1873").AutoFilter Field:=1, Criteria1:="ОтвСтр"
+ActiveSheet.Range("$A$1:$G$1873").AutoFilter Field:=2, Criteria1:="ЮЛ"
+ActiveSheet.Range("$A$1:$G$1873").AutoFilter Field:=3, Criteria1:="Первоначальный"
+Range("D1:G1").Select
+Range(Selection, Selection.End(xlDown)).Select
+Application.CutCopyMode = False
+Selection.Copy
+Sheets("Data").Select
+Range("A1").Select
+Selection.PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks:=False, Transpose:=False
+
+Calc_Ind
+Расчет
+
+Sheets("Result").Select
+Range("A2:D14").Select
+Selection.Copy
+Sheets("ResultFull").Select
+Range("A52").Select
+Selection.PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks:=False, Transpose:=False
+
+' Расчет 5: ответственный страховщик, ЮЛ, возобновлённые
+
+Sheets("DataFull").Select
+ActiveSheet.Range("$A$1:$G$1873").AutoFilter Field:=1, Criteria1:="ОтвСтр"
+ActiveSheet.Range("$A$1:$G$1873").AutoFilter Field:=2, Criteria1:="ЮЛ"
+ActiveSheet.Range("$A$1:$G$1873").AutoFilter Field:=3, Criteria1:="Возобновлённый"
+Range("D1:G1").Select
+Range(Selection, Selection.End(xlDown)).Select
+Application.CutCopyMode = False
+Selection.Copy
+Sheets("Data").Select
+Range("A1").Select
+Selection.PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks:=False, Transpose:=False
+
+Calc_Ind
+Расчет
+
+Sheets("Result").Select
+Range("A2:D14").Select
+Selection.Copy
+Sheets("ResultFull").Select
+Range("A68").Select
+Selecti
