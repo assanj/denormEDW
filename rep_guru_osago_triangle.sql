e11685a80ad50a02b4a03755844e0f884b44929f
AssanJ <84403654+assanj@users.noreply.github.com>
2026-09-02 13:15:44 +0300

    Update rep_guru_osago_triangle.sql

--- rep_guru_osago_triangle.sql
+++ rep_guru_osago_triangle.sql
@@ -1,4 +1,24 @@
-SQL Error [42601]: ERROR: "v_s1" is not a known variable
-  Position: 3109
-  ERROR: "v_s1" is not a known variable
-  Position: 3109
+SQL Error [22012]: ERROR: division by zero
+  Where: SQL statement "SELECT 
+                    COALESCE(SUM(s / v_k1_curr[v_i]), 0), -- Сумма S / K1
+                    COALESCE(SUM(v), 0)                   -- Сумма V
+                                                 FROM temp_matrix
+                WHERE "Месяц действия" = v_action_month
+                  AND "Месяц календарный" = v_calendar_month"
+PL/pgSQL function glm_calc_main() line 141 at SQL statement
+SQL statement "SELECT actuary.glm_calc_main()"
+PL/pgSQL function glm_calc_all() line 108 at PERFORM
+SQL statement "SELECT actuary.glm_calc_all()"
+PL/pgSQL function glm_run_all() line 16 at PERFORM
+  ERROR: division by zero
+  Where: SQL statement "SELECT 
+                    COALESCE(SUM(s / v_k1_curr[v_i]), 0), -- Сумма S / K1
+                    COALESCE(SUM(v), 0)                   -- Сумма V
+                                                 FROM temp_matrix
+                WHERE "Месяц действия" = v_action_month
+                  AND "Месяц календарный" = v_calendar_month"
+PL/pgSQL function glm_calc_main() line 141 at SQL statement
+SQL statement "SELECT actuary.glm_calc_main()"
+PL/pgSQL function glm_calc_all() line 108 at PERFORM
+SQL statement "SELECT actuary.glm_calc_all()"
+PL/pgSQL function glm_run_all() line 16 at PERFORM
