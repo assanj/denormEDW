4b381afd297129a412ffbba23e859a778b404244
AssanJ <84403654+assanj@users.noreply.github.com>
2026-09-07 01:05:18 +0300

    Update new_sql.sql

--- new_sql.sql
+++ new_sql.sql
@@ -1,29 +1,15 @@
-' Добавьте эту переменную в начало процедуры
-Dim foundCount As Integer
+' После инициализации всех массивов
+For i = 0 To N1 - 1
+    tempK1(i) = 1
+Next i
+For j = 0 To N2 - 1
+    tempK2(j) = 1
+Next j
+For j = 0 To N2 - 1
+    K2(j) = 1
+Next j
 
-' Инициализируйте перед циклом
-foundCount = 0
-
-' Внутри цикла, где заполняются матрицы:
-For num = 2 To Count
-    g1 = Worksheets("Data").Cells(num, 1).Value
-    g2 = Worksheets("Data").Cells(num, 2).Value
-    For i = 0 To N1 - 1
-        For j = 0 To N2 - 1
-            If g1 = F1(i) And g2 = F2(j) Then
-                foundCount = foundCount + 1  ' <-- ДОБАВЬТЕ ЭТУ СТРОКУ
-                V(i, j) = Worksheets("Data").Cells(num, 4).Value
-                S(i, j) = Worksheets("Data").Cells(num, 3).Value
-                If V(i, j) > 0 Then
-                    Z(i, j) = S(i, j) / V(i, j)
-                Else
-                    Z(i, j) = 0
-                End If
-            End If
-        Next j
-    Next i
-Next num
-
-' После цикла добавьте:
-MsgBox "Найдено соответствий: " & foundCount & vbCrLf & _
-       "Ожидалось: " & N1 * N2 & " (13*12=156)"
+' ДОБАВЬТЕ ПРОВЕРКУ:
+MsgBox "tempK1(0)=" & tempK1(0) & ", tempK1(12)=" & tempK1(12) & vbCrLf & _
+       "tempK2(0)=" & tempK2(0) & ", tempK2(11)=" & tempK2(11) & vbCrLf & _
+       "K2(0)=" & K2(0) & ", K2(11)=" & K2(11)
