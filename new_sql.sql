d997567f2882b2679664a7cb1851ebada5a35fc7
AssanJ <84403654+assanj@users.noreply.github.com>
2026-09-07 00:09:38 +0300

    Update new_sql.sql

--- new_sql.sql
+++ new_sql.sql
@@ -8,40 +8,44 @@ DECLARE
     v_eps NUMERIC := 0.000001;
     v_dist NUMERIC := 1;
     v_iter INTEGER := 0;
-    v_max_iter INTEGER := 10000;
+    v_max_iter INTEGER := 1000;
     v_i INTEGER;
     v_j INTEGER;
-    v_num INTEGER;
-    v_s1 NUMERIC;
-    v_s2 NUMERIC;
-    v_vi NUMERIC;
-    v_si NUMERIC;
     v_action_months INTEGER[];
     v_calendar_months INTEGER[];
     v_action_month INTEGER;
     v_calendar_month INTEGER;
     
-    -- Массивы для V и Z матриц
+    -- Массивы для данных
     v_v NUMERIC[][];
-    v_z NUMERIC[][];
     v_s NUMERIC[][];
+    v_z NUMERIC[][];
     
-    -- Массивы для промежуточных расчетов
-    v_v_plus_i NUMERIC[];
+    -- Массивы для индексов
+    v_k1_prev NUMERIC[];
+    v_k1_curr NUMERIC[];
+    v_k2_prev NUMERIC[];
+    v_k2_curr NUMERIC[];
+    
+    -- Временные массивы для расчетов
     v_v_i_plus NUMERIC[];
-    v_z_plus_i NUMERIC[];
+    v_v_j_plus NUMERIC[];
     v_z_i_plus NUMERIC[];
+    v_z_j_plus NUMERIC[];
     v_z_i NUMERIC[];
+    v_z_j NUMERIC[];
     
-    v_ind1 NUMERIC[];
-    v_ind2 NUMERIC[];
+    v_sum1 NUMERIC;
+    v_sum2 NUMERIC;
+    v_sum3 NUMERIC;
+    v_vi NUMERIC;
+    v_si NUMERIC;
 BEGIN
-    -- Получаем уникальные месяцы действия
+    -- Получаем уникальные месяцы
     SELECT ARRAY_AGG(DISTINCT "Месяц действия" ORDER BY "Месяц действия")
     INTO v_action_months
     FROM actuary.glm_data;
     
-    -- Получаем уникальные календарные месяцы
     SELECT ARRAY_AGG(DISTINCT "Месяц календарный" ORDER BY "Месяц календарный")
     INTO v_calendar_months
     FROM actuary.glm_data;
@@ -56,8 +60,8 @@ BEGIN
     
     -- Инициализация матриц
     v_v := array_fill(0, ARRAY[v_n1, v_n2]);
-    v_z := array_fill(0, ARRAY[v_n1, v_n2]);
     v_s := array_fill(0, ARRAY[v_n1, v_n2]);
+    v_z := array_fill(0, ARRAY[v_n1, v_n2]);
     
     -- Заполняем матрицы из данных
     FOR v_i IN 1..v_n1 LOOP
@@ -83,163 +87,176 @@ BEGIN
         END LOOP;
     END LOOP;
     
-    -- Инициализация массивов
-    v_ind1 := array_fill(0, ARRAY[v_n1]);
-    v_ind2 := array_fill(0, ARRAY[v_n2]);
+    -- Инициализация массивов индексов
+    v_k1_prev := array_fill(1, ARRAY[v_n1]);
+    v_k1_curr := array_fill(1, ARRAY[v_n1]);
+    v_k2_prev := array_fill(1, ARRAY[v_n2]);
+    v_k2_curr := array_fill(1, ARRAY[v_n2]);
     
-    RAISE NOTICE 'Начинаем расчет (N1=%, N2=%)...', v_n1, v_n2;
+    RAISE NOTICE 'Начинаем итеративный расчет (N1=%, N2=%)...', v_n1, v_n2;
     
-    -- =====================================================
-    -- Расчет Ind1 (по месяцам действия)
-    -- =====================================================
-    -- Инициализация массивов для Ind1
-    v_v_plus_i := array_fill(0, ARRAY[v_n2]);
-    v_v_i_plus := array_fill(0, ARRAY[v_n1]);
-    v_z_plus_i := array_fill(0, ARRAY[v_n2]);
-    v_z_i_plus := array_fill(0, ARRAY[v_n1]);
-    v_z_i := array_fill(0, ARRAY[v_n1]);
-    
-    -- Для каждой строки (месяца действия)
-    FOR v_num IN 1..v_n1 LOOP
-        -- 1. Вычисляем V_plus_i(j) - сумма V по каждому столбцу
-        FOR v_j IN 1..v_n2 LOOP
-            v_v_plus_i[v_j] := 0;
-            FOR v_i IN 1..v_n1 LOOP
-                v_v_plus_i[v_j] := v_v_plus_i[v_j] + v_v[v_i][v_j];
-            END LOOP;
-        END LOOP;
+    -- Итеративный процесс
+    WHILE v_dist > v_eps AND v_iter < v_max_iter LOOP
+        v_iter := v_iter + 1;
         
-        -- 2. Вычисляем V_i_plus(num) - сумма V по текущей строке
-        v_v_i_plus[v_num] := 0;
-        FOR v_j IN 1..v_n2 LOOP
-            v_v_i_plus[v_num] := v_v_i_plus[v_num] + v_v[v_num][v_j];
-        END LOOP;
-        
-        -- 3. Вычисляем Z_plus_i(j) - взвешенное среднее Z по каждому столбцу
-        FOR v_j IN 1..v_n2 LOOP
-            v_z_plus_i[v_j] := 0;
-            FOR v_i IN 1..v_n1 LOOP
-                v_z_plus_i[v_j] := v_z_plus_i[v_j] + v_v[v_i][v_j] * v_z[v_i][v_j];
+        -- 1. Обновляем K1 (по месяцам действия)
+        FOR v_i IN 1..v_n1 LOOP
+            -- Вычисляем V_i_plus - сумма по строке
+            v_sum2 := 0;
+            FOR v_j IN 1..v_n2 LOOP
+                v_sum2 := v_sum2 + v_v[v_i][v_j];
             END LOOP;
-            IF v_v_plus_i[v_j] > 0 THEN
-                v_z_plus_i[v_j] := v_z_plus_i[v_j] / v_v_plus_i[v_j];
+            
+            -- Вычисляем Z_i_plus - взвешенное среднее по строке
+            v_sum1 := 0;
+            FOR v_j IN 1..v_n2 LOOP
+                IF v_v[v_i][v_j] > 0 THEN
+                    v_sum1 := v_sum1 + v_v[v_i][v_j] * (v_s[v_i][v_j] / v_v[v_i][v_j]);
+                END IF;
+            END LOOP;
+            
+            IF v_sum2 > 0 THEN
+                v_z_i_plus := v_sum1 / v_sum2;
             ELSE
-                v_z_plus_i[v_j] := 0;
+                v_z_i_plus := 0;
+            END IF;
+            
+            -- Вычисляем Z_plus_i для каждого столбца
+            v_z_i := 0;
+            FOR v_j IN 1..v_n2 LOOP
+                -- Сначала вычисляем Z_plus_i(j) - взвешенное среднее по столбцу j
+                v_sum1 := 0;
+                v_sum2 := 0;
+                FOR v_ii IN 1..v_n1 LOOP
+                    IF v_v[v_ii][v_j] > 0 THEN
+                        v_sum1 := v_sum1 + v_v[v_ii][v_j] * (v_s[v_ii][v_j] / v_v[v_ii][v_j]);
+                        v_sum2 := v_sum2 + v_v[v_ii][v_j];
+                    END IF;
+                END LOOP;
+                
+                IF v_sum2 > 0 THEN
+                    v_z_plus_i := v_sum1 / v_sum2;
+                ELSE
+                    v_z_plus_i := 0;
+                END IF;
+                
+                -- Добавляем к Z_i
+                v_z_i := v_z_i + v_v[v_i][v_j] * v_z_plus_i;
+            END LOOP;
+            
+            -- Нормализуем Z_i
+            v_sum2 := 0;
+            FOR v_j IN 1..v_n2 LOOP
+                v_sum2 := v_sum2 + v_v[v_i][v_j];
+            END LOOP;
+            
+            IF v_sum2 > 0 THEN
+                v_z_i := v_z_i / v_sum2;
+            ELSE
+                v_z_i := 1;
+            END IF;
+            
+            -- Обновляем K1
+            IF v_z_i > 0 THEN
+                v_k1_curr[v_i] := v_z_i_plus / v_z_i;
+            ELSE
+                v_k1_curr[v_i] := 1;
             END IF;
         END LOOP;
         
-        -- 4. Вычисляем Z_i_plus(num) - взвешенное среднее Z по текущей строке
-        v_z_i_plus[v_num] := 0;
+        -- 2. Обновляем K2 (по календарным месяцам)
         FOR v_j IN 1..v_n2 LOOP
-            v_z_i_plus[v_num] := v_z_i_plus[v_num] + v_v[v_num][v_j] * v_z[v_num][v_j];
-        END LOOP;
-        IF v_v_i_plus[v_num] > 0 THEN
-            v_z_i_plus[v_num] := v_z_i_plus[v_num] / v_v_i_plus[v_num];
-        ELSE
-            v_z_i_plus[v_num] := 0;
-        END IF;
-        
-        -- 5. Вычисляем Z_i(num) - взвешенное среднее Z_plus_i по текущей строке
-        v_z_i[v_num] := 0;
-        FOR v_j IN 1..v_n2 LOOP
-            v_z_i[v_num] := v_z_i[v_num] + v_v[v_num][v_j] * v_z_plus_i[v_j];
-        END LOOP;
-        IF v_v_i_plus[v_num] > 0 THEN
-            v_z_i[v_num] := v_z_i[v_num] / v_v_i_plus[v_num];
-        ELSE
-            v_z_i[v_num] := 0;
-        END IF;
-        
-        -- 6. Вычисляем Ind1(num)
-        IF v_z_i[v_num] > 0 THEN
-            v_ind1[v_num] := v_z_i_plus[v_num] / v_z_i[v_num];
-        ELSE
-            v_ind1[v_num] := 0;
-        END IF;
-    END LOOP;
-    
-    -- =====================================================
-    -- Расчет Ind2 (по календарным месяцам)
-    -- =====================================================
-    -- Переинициализация массивов для Ind2
-    v_v_plus_i := array_fill(0, ARRAY[v_n1]);
-    v_v_i_plus := array_fill(0, ARRAY[v_n2]);
-    v_z_plus_i := array_fill(0, ARRAY[v_n1]);
-    v_z_i_plus := array_fill(0, ARRAY[v_n2]);
-    v_z_i := array_fill(0, ARRAY[v_n2]);
-    
-    -- Для каждого столбца (календарного месяца)
-    FOR v_num IN 1..v_n2 LOOP
-        -- 1. Вычисляем V_plus_i(i) - сумма V по каждой строке
-        FOR v_i IN 1..v_n1 LOOP
-            v_v_plus_i[v_i] := 0;
-            FOR v_j IN 1..v_n2 LOOP
-                v_v_plus_i[v_i] := v_v_plus_i[v_i] + v_v[v_i][v_j];
+            -- Вычисляем V_j_plus - сумма по столбцу
+            v_sum2 := 0;
+            FOR v_i IN 1..v_n1 LOOP
+                v_sum2 := v_sum2 + v_v[v_i][v_j];
             END LOOP;
-        END LOOP;
-        
-        -- 2. Вычисляем V_i_plus(num) - сумма V по текущему столбцу
-        v_v_i_plus[v_num] := 0;
-        FOR v_i IN 1..v_n1 LOOP
-            v_v_i_plus[v_num] := v_v_i_plus[v_num] + v_v[v_i][v_num];
-        END LOOP;
-        
-        -- 3. Вычисляем Z_plus_i(i) - взвешенное среднее Z по каждой строке
-        FOR v_i IN 1..v_n1 LOOP
-            v_z_plus_i[v_i] := 0;
-            FOR v_j IN 1..v_n2 LOOP
-                v_z_plus_i[v_i] := v_z_plus_i[v_i] + v_v[v_i][v_j] * v_z[v_i][v_j];
+            
+            -- Вычисляем Z_j_plus - взвешенное среднее по столбцу
+            v_sum1 := 0;
+            FOR v_i IN 1..v_n1 LOOP
+                IF v_v[v_i][v_j] > 0 THEN
+                    v_sum1 := v_sum1 + v_v[v_i][v_j] * (v_s[v_i][v_j] / v_v[v_i][v_j]);
+                END IF;
             END LOOP;
-            IF v_v_plus_i[v_i] > 0 THEN
-                v_z_plus_i[v_i] := v_z_plus_i[v_i] / v_v_plus_i[v_i];
+            
+            IF v_sum2 > 0 THEN
+                v_z_j_plus := v_sum1 / v_sum2;
             ELSE
-                v_z_plus_i[v_i] := 0;
+                v_z_j_plus := 0;
+            END IF;
+            
+            -- Вычисляем Z_plus_j для каждой строки
+            v_z_j := 0;
+            FOR v_i IN 1..v_n1 LOOP
+                -- Сначала вычисляем Z_plus_j(i) - взвешенное среднее по строке i
+                v_sum1 := 0;
+                v_sum2 := 0;
+                FOR v_jj IN 1..v_n2 LOOP
+                    IF v_v[v_i][v_jj] > 0 THEN
+                        v_sum1 := v_sum1 + v_v[v_i][v_jj] * (v_s[v_i][v_jj] / v_v[v_i][v_jj]);
+                        v_sum2 := v_sum2 + v_v[v_i][v_jj];
+                    END IF;
+                END LOOP;
+                
+                IF v_sum2 > 0 THEN
+                    v_z_plus_j := v_sum1 / v_sum2;
+                ELSE
+                    v_z_plus_j := 0;
+                END IF;
+                
+                -- Добавляем к Z_j
+                v_z_j := v_z_j + v_v[v_i][v_j] * v_z_plus_j;
+            END LOOP;
+            
+            -- Нормализуем Z_j
+            v_sum2 := 0;
+            FOR v_i IN 1..v_n1 LOOP
+                v_sum2 := v_sum2 + v_v[v_i][v_j];
+            END LOOP;
+            
+            IF v_sum2 > 0 THEN
+                v_z_j := v_z_j / v_sum2;
+            ELSE
+                v_z_j := 1;
+            END IF;
+            
+            -- Обновляем K2
+            IF v_z_j > 0 THEN
+                v_k2_curr[v_j] := v_z_j_plus / v_z_j;
+            ELSE
+                v_k2_curr[v_j] := 1;
             END IF;
         END LOOP;
         
-        -- 4. Вычисляем Z_i_plus(num) - взвешенное среднее Z по текущему столбцу
-        v_z_i_plus[v_num] := 0;
-        FOR v_i IN 1..v_n1 LOOP
-            v_z_i_plus[v_num] := v_z_i_plus[v_num] + v_v[v_i][v_num] * v_z[v_i][v_num];
-        END LOOP;
-        IF v_v_i_plus[v_num] > 0 THEN
-            v_z_i_plus[v_num] := v_z_i_plus[v_num] / v_v_i_plus[v_num];
-        ELSE
-            v_z_i_plus[v_num] := 0;
-        END IF;
+        -- Вычисляем ошибку
+        v_dist := actuary.glm_norma(
+            v_k1_curr, v_k1_prev,
+            v_k2_curr, v_k2_prev,
+            v_n1, v_n2
+        );
         
-        -- 5. Вычисляем Z_i(num) - взвешенное среднее Z_plus_i по текущему столбцу
-        v_z_i[v_num] := 0;
-        FOR v_i IN 1..v_n1 LOOP
-            v_z_i[v_num] := v_z_i[v_num] + v_v[v_i][v_num] * v_z_plus_i[v_i];
-        END LOOP;
-        IF v_v_i_plus[v_num] > 0 THEN
-            v_z_i[v_num] := v_z_i[v_num] / v_v_i_plus[v_num];
-        ELSE
-            v_z_i[v_num] := 0;
-        END IF;
+        v_k1_prev := v_k1_curr;
+        v_k2_prev := v_k2_curr;
         
-        -- 6. Вычисляем Ind2(num)
-        IF v_z_i[v_num] > 0 THEN
-            v_ind2[v_num] := v_z_i_plus[v_num] / v_z_i[v_num];
-        ELSE
-            v_ind2[v_num] := 0;
+        IF v_iter % 10 = 0 THEN
+            RAISE NOTICE 'Итерация %, ошибка: %', v_iter, v_dist;
         END IF;
     END LOOP;
+    
+    RAISE NOTICE 'Расчет завершен. Итераций: %, ошибка: %', v_iter, v_dist;
     
     -- Сохраняем результаты
     FOR v_i IN 1..v_n1 LOOP
         INSERT INTO actuary.glm_result ("Месяц действия", "K1")
-        VALUES (v_action_months[v_i], v_ind1[v_i]);
+        VALUES (v_action_months[v_i], v_k1_curr[v_i]);
     END LOOP;
     
     FOR v_j IN 1..v_n2 LOOP
         INSERT INTO actuary.glm_result ("Месяц календарный", "K2")
-        VALUES (v_calendar_months[v_j], v_ind2[v_j]);
+        VALUES (v_calendar_months[v_j], v_k2_curr[v_j]);
     END LOOP;
     
-    UPDATE actuary.glm_result SET "Базовая частота" = 1;
-    
-    RAISE NOTICE 'Расчет завершен.';
+    RAISE NOTICE 'Результаты сохранены.';
 END;
 $function$;
