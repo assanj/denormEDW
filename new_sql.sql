ec7de09154033712815f25a96369e1c98148f46b
AssanJ <84403654+assanj@users.noreply.github.com>
2026-09-07 01:02:12 +0300

    Update new_sql.sql

--- new_sql.sql
+++ new_sql.sql
@@ -1,13 +1,29 @@
-' После заполнения F1
-For i = 0 To N1 - 1
-    F1(i) = CStr(Worksheets("Result").Cells(i + 2, 1).Value)
-Next i
+' Добавьте эту переменную в начало процедуры
+Dim foundCount As Integer
 
-' После заполнения F2
-For i = 0 To N2 - 1
-    F2(i) = CStr(Worksheets("Result").Cells(i + 2, 3).Value)
-Next i
+' Инициализируйте перед циклом
+foundCount = 0
 
-' ДОБАВЬТЕ ПРОВЕРКУ:
-MsgBox "F1(0)=" & F1(0) & ", F1(12)=" & F1(12) & vbCrLf & _
-       "F2(0)=" & F2(0) & ", F2(11)=" & F2(11)
+' Внутри цикла, где заполняются матрицы:
+For num = 2 To Count
+    g1 = Worksheets("Data").Cells(num, 1).Value
+    g2 = Worksheets("Data").Cells(num, 2).Value
+    For i = 0 To N1 - 1
+        For j = 0 To N2 - 1
+            If g1 = F1(i) And g2 = F2(j) Then
+                foundCount = foundCount + 1  ' <-- ДОБАВЬТЕ ЭТУ СТРОКУ
+                V(i, j) = Worksheets("Data").Cells(num, 4).Value
+                S(i, j) = Worksheets("Data").Cells(num, 3).Value
+                If V(i, j) > 0 Then
+                    Z(i, j) = S(i, j) / V(i, j)
+                Else
+                    Z(i, j) = 0
+                End If
+            End If
+        Next j
+    Next i
+Next num
+
+' После цикла добавьте:
+MsgBox "Найдено соответствий: " & foundCount & vbCrLf & _
+       "Ожидалось: " & N1 * N2 & " (13*12=156)"
