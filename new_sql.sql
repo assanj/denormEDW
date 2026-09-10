e64b3e947611450ec29243c6d7a40c453200f324
AssanJ <84403654+assanj@users.noreply.github.com>
2026-09-11 01:18:33 +0300

    Update new_sql.sql

--- new_sql.sql
+++ new_sql.sql
@@ -1,131 +1,67 @@
-PROCEDURE XLS_EXPOSISION_PREMIUM_KASKO_O is
--- запускаем только после "Сформированных основных форм" и "Рассчитанного агрегата" по КАСКО
--- в "Актуарном отчетном модуле" + GWPC Каско!!!,
--- результат вставляем на соответствующий лист в xlsb "YYYY-MM-DD - Экспозиция и ЗП по ОСАГО и КАСКО.xlsb"
-
-  v_proc varchar2(128) := 'XLS_EXPOSISION_PREMIUM_KASKO_O';
-  v_scri varchar2(128) := 'YYYY-MM-DD - Экспозиция и ЗП по КАСКО для Ольги.sql';
-
-begin
-utils.logz(v_proc, v_scri, 'START');
-
-execute immediate 'truncate table XLS_EXPOSISION_PREMIUM_KASKO_O';
-
--- собираем и агрегируем экспозицию по GWPC+АРМ+ЕКИС:
-
--- если закрываем квартал:
-if trunc(sysdate,'MM')-1 = trunc(sysdate,'q')-1 then
-   utils.logz(v_proc, v_scri, 'квартал');
-ELSE
-   utils.logz(v_proc, v_scri, 'месяц');
-END IF;
-
 insert --+ APPEND
   into XLS_EXPOSISION_PREMIUM_KASKO_O
 SELECT /*+ parallel(krf 16) */
-       krf.db,
-       krf.NEW_REGIONNAME,
-       krf.PROPERTYFORMNAME,
-       krf.PRODUCT_PROGRAM_TYPE,
-       krf.OBJECTCLASS,
-       krf.is_dead_policy,
-       CH.SALECHANNELFULLNAME_DE,
-       krf.category_id_2020,
-       to_char(krf.T1, 'yyyymm') YEAR_MONTH,
-       to_char(krf.T1, 'yyyyq') YEAR_QUARTER,
-       CASE WHEN krf.PROPERTYFORMNAME = 'Физическое лицо' THEN
-            CASE WHEN krf.kasko_chain_number > 1 THEN 'Возобновлённый'
-                 ELSE 'Первоначальный'
-            END
-            ELSE
-            CASE krf.CONTRACTOPTION
-                  WHEN '1' THEN 'Первоначальный'
-                  WHEN '2' THEN 'Возобновлённый'
-                  ELSE 'нд'
-            END
-       END as IS_PROLONGATION,
-       SUM(krf.EXPOSURE) EXPOSURE,
-       SUM(krf.AC_EXPOSURE) AC_EXPOSURE,
-       SUM(krf.AC_EXPOSURE60) AC_EXPOSURE60,
-       SUM(krf.DE_ZPOP60_INITIAL - krf.RETURNPREMIUM) DE_ZPOP60_INITIAL,
-       SUM(krf.POLICYCOUNT) POLICYCOUNT,
-       SUM(krf.WRITTENPREMIUM) WP,
-       SUM(krf.WRITTENPREMIUM60) WP60,
-       SUM(krf.OBJECTQUANTITY) OBJECTQUANTITY,
-       SUM(krf.RETURNPREMIUM) RETURNPREMIUM,
-       SUM(krf.LIABILITY) LIABILITY
+       krf.db                                              as "База данных"
+      ,krf.NEW_REGIONNAME                                  as "Регион"
+      ,CASE WHEN krf.PROPERTYFORMNAME = 'Физическое лицо'
+            THEN 'ФЛ' ELSE 'ЮЛ'
+       END                                                 as "Тип страхователя"
+      ,CASE WHEN krf.OBJECTCLASS = 'ТС'
+            THEN 'ТС' ELSE 'Иное'
+       END                                                 as "Тип объекта"
+      ,krf.PROPERTYFORMNAME                                as "PROPERTYFORMNAME"
+      ,CH.SALECHANNELFULLNAME_DE                           as "Канал продаж"
+      ,krf.SALECHANNEL2008                                 as "SALECHANNEL2008"
+      ,krf.IS_DEAD_POLICY                                  as "IS_DEAD_POLICY"
+      ,krf.OBJECTCLASS                                     as "OBJECTCLASS"
+      ,krf.PRODUCT_PROGRAM_TYPE                            as "PRODUCT_PROGRAM_TYPE"
+      ,nvl(krf.FRANCHIZE_TYPE, 'Нет')                      as "FRANCHISE_TYPE"
+      ,CASE krf.CONTRACTOPTION
+            WHEN '2' THEN 'Возобновлённый'
+            ELSE 'Первоначальный'
+       END                                                 as "Признак договора"
+      ,krf.D1QUARTER                                       as "Квартал начала"
+      ,krf.D1MONTH                                         as "Месяц начала"
+      ,to_char(krf.T1, 'yyyyq')                            as "Квартал"
+      ,to_char(krf.T1, 'yyyymm')                           as "Месяц"
+      ,SUM(krf.AC_EXPOSURE60)                              as "Экспозиция"
+      ,SUM(krf.DE_ZPOP60_INITIAL - krf.RETURNPREMIUM)      as "Заработанная премия"
+      ,SUM(krf.POLICYCOUNT)                                as "Количество договоров"
+      ,SUM(krf.WRITTENPREMIUM)                             as "НП"
+      ,SUM(krf.OBJECTQUANTITY)                             as "Количество объектов"
+      ,SUM(krf.WRITTENPREMIUM60)                           as "НПОП60"
+      ,SUM(krf.PROPERTYLIABILITYEXPOSURE)                  as "СС_экспозиция"
+      ,SUM(krf.LIABILITY * krf.OBJECTQUANTITY)             as "СС_перв"
 FROM KASKO_RESULT_FULL_PARTS krf
-LEFT JOIN DICTSALECHANNELTYPE2008 ch ON CH.SALE_CHANNEL_TYPE2008_ID = krf.SALECHANNEL2008
+LEFT JOIN DICTSALECHANNELTYPE2008 ch
+       ON CH.SALE_CHANNEL_TYPE2008_ID = krf.SALECHANNEL2008
 WHERE krf.part = CASE
-                    WHEN TRUNC(SYSDATE, 'MM') - 1 = TRUNC(SYSDATE, 'Q') - 1 THEN 'q'  -- квартал
-                    ELSE 'm'  -- месяц
+                    WHEN TRUNC(SYSDATE, 'MM') - 1 = TRUNC(SYSDATE, 'Q') - 1 THEN 'q'
+                    ELSE 'm'
                  END
+  AND (krf.db <> 'ЕКИС' OR krf.BRANCH_CHAR_ID <> 17701010)
 GROUP BY
-    krf.db,
-    krf.NEW_REGIONNAME,
-    krf.PROPERTYFORMNAME,
-    krf.PRODUCT_PROGRAM_TYPE,
-    krf.OBJECTCLASS,
-    krf.is_dead_policy,
-    CH.SALECHANNELFULLNAME_DE,
-    krf.category_id_2020,
-    to_char(krf.T1, 'yyyymm'),
-    to_char(krf.T1, 'yyyyq'),
-    CASE WHEN krf.PROPERTYFORMNAME = 'Физическое лицо' THEN
-         CASE WHEN krf.kasko_chain_number > 1 THEN 'Возобновлённый'
-              ELSE 'Первоначальный'
-         END
-         ELSE
-         CASE krf.CONTRACTOPTION
-               WHEN '1' THEN 'Первоначальный'
-               WHEN '2' THEN 'Возобновлённый'
-               ELSE 'нд'
-         END
-    END
+       krf.db
+      ,krf.NEW_REGIONNAME
+      ,CASE WHEN krf.PROPERTYFORMNAME = 'Физическое лицо'
+            THEN 'ФЛ' ELSE 'ЮЛ'
+       END
+      ,CASE WHEN krf.OBJECTCLASS = 'ТС'
+            THEN 'ТС' ELSE 'Иное'
+       END
+      ,krf.PROPERTYFORMNAME
+      ,CH.SALECHANNELFULLNAME_DE
+      ,krf.SALECHANNEL2008
+      ,krf.IS_DEAD_POLICY
+      ,krf.OBJECTCLASS
+      ,krf.PRODUCT_PROGRAM_TYPE
+      ,nvl(krf.FRANCHIZE_TYPE, 'Нет')
+      ,CASE krf.CONTRACTOPTION
+            WHEN '2' THEN 'Возобновлённый'
+            ELSE 'Первоначальный'
+       END
+      ,krf.D1QUARTER
+      ,krf.D1MONTH
+      ,to_char(krf.T1, 'yyyyq')
+      ,to_char(krf.T1, 'yyyymm')
 ORDER BY 1, 2, 3, 4;
-
-utils.logz(v_proc, v_scri, 'END. считали экспозицию по Каско за '||CASE
-                    WHEN TRUNC(SYSDATE, 'MM') - 1 = TRUNC(SYSDATE, 'Q') - 1 THEN 'Квартал'
-                    ELSE 'Месяц'
-                 END);
-COMMIT;
-
--- вставляем данные прошлых периодов из ранее расчитанного агрегата
--- (по аналогии с OSAGO_O):
-insert --+ APPEND
-  into XLS_EXPOSISION_PREMIUM_KASKO_O
-select
-       DB,
-       NEW_REGIONNAME,
-       PROPERTYFORMNAME,
-       PRODUCT_PROGRAM_TYPE,
-       OBJECTCLASS,
-       IS_DEAD_POLICY,
-       SALECHANNELFULLNAME_DE,
-       CATEGORY_ID_2020,
-       YEAR_MONTH,
-       YEAR_QUARTER,
-       'нд' as IS_PROLONGATION,
-       EXPOSURE,
-       AC_EXPOSURE,
-       AC_EXPOSURE60,
-       DE_ZPOP60_INITIAL,
-       POLICYCOUNT,
-       WP,
-       WP60,
-       OBJECTQUANTITY,
-       RETURNPREMIUM,
-       LIABILITY
-from XLS_EXPOSISION_PREMIUM_KASKO16 ex16
- where ex16.YEAR_QUARTER < (select min(YEAR_QUARTER) from XLS_EXPOSISION_PREMIUM_KASKO_O);
-
-utils.logz(v_proc, v_scri, 'данные прошлых периодов');
-COMMIT;
-
-utils.logz(v_proc, v_scri, 'END');
-
-utils.email(v_proc, v_scri || ' accomplished', 'Anastasiya_Zhukova@rgs.ru');
-
-EXCEPTION  when others then  utils.logz(v_proc, v_scri, sqlerrm || chr(10) || chr(13) || dbms_utility.format_error_backtrace || chr(10) || chr(13) || DBMS_UTILITY.format_call_stack );
-RAISE;
-END;--XLS_EXPOSISION_PREMIUM_KASKO_O
